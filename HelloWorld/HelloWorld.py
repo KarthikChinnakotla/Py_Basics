d41170488f8852502c23eea383e8ffd69d78c58c
KarthikChinnakotla <g_saikarthik@yahoo.co.in>
2022-07-15 22:07:06 +0530

    Updated

--- HelloWorld/HelloWorld.py
+++ HelloWorld/HelloWorld.py
@@ -1,1 +1,11 @@
-print ("First Program")
+# First Python program
+# # indicates single line comment
+
+"""
+
+Triple quotes indicate
+multi line comments
+
+"""
+
+print ("First Program in Python - Hello World")
