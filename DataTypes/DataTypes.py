be0999a5c71ab8ba22dc0281e603d15739923a74
KarthikChinnakotla <g_saikarthik@yahoo.co.in>
2022-07-15 22:17:28 +0530

    Initial version

---
+++ DataTypes/DataTypes.py
@@ -0,0 +1,13 @@
+"""
+Second program - Data type usage in python
+
+python 3.x: requires brackets () for print
+"""
+
+integerVal = 100
+stringVar = "Hello World"
+floatVal = 50.045
+
+print (integerVal)
+print (stringVar)
+print (floatVal)
